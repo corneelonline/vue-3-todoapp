2b2e98e5c34563a9765600517757722e8c2d7cbe
Marco Verheul <marco@corneelonline.nl>
2022-10-11 09:28:24 +0200

    Add structure for router-views for different kind of page layouts

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <script setup>
+import { RouterView } from "vue-router";
 import AppHeader from "./components/AppHeader.vue";
-import AppSidebar from "./components/AppSidebar.vue";
-import { useSidebarOpen } from "./composables/useSidebarOpen.js";
-const { isSidebarOpen } = useSidebarOpen();
 </script>
 
 <template>
   <div class="page">
     <AppHeader />
-    <div
-      class="wrapper"
-      :class="isSidebarOpen ? 'sidebar-open' : 'sidebar-closed'"
-    >
-      <AppSidebar />
-      <main id="main">
-        <RouterView />
-      </main>
-    </div>
+    <RouterView />
   </div>
 </template>
 
@@ -25,19 +15,5 @@ const { isSidebarOpen } = useSidebarOpen();
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-}
-.wrapper {
-  display: flex;
-  min-height: calc(100vh - 50px);
-}
-#main {
-  background-color: var(--color-bg-body);
-  padding: var(--gutter-md);
-}
-.sidebar-open #main {
-  width: calc(100% - 270px);
-}
-.sidebar-closed #main {
-  width: 100%;
 }
 </style>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,18 +1,34 @@
 <script setup>
 import AppSidebar from "../components/AppSidebar.vue";
+import { useSidebarOpen } from "../composables/useSidebarOpen.js";
+const { isSidebarOpen } = useSidebarOpen();
 </script>
 
 <template>
-  <div class="page">
-    <AppHeader />
-    <div
-      class="wrapper"
-      :class="isSidebarOpen ? 'sidebar-open' : 'sidebar-closed'"
-    >
-      <AppSidebar />
-      <main id="main">
-        <slot />
-      </main>
-    </div>
+  <div
+    class="wrapper"
+    :class="isSidebarOpen ? 'sidebar-open' : 'sidebar-closed'"
+  >
+    <AppSidebar />
+    <main id="main">
+      <h1>ListView</h1>
+    </main>
   </div>
 </template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  min-height: calc(100vh - 50px);
+}
+#main {
+  background-color: var(--color-bg-body);
+  padding: var(--gutter-md);
+}
+.sidebar-open #main {
+  width: calc(100% - 270px);
+}
+.sidebar-closed #main {
+  width: 100%;
+}
+</style>
