ec57c4ba547a33785433f70b4605e5a21dcf8695
Marco Verheul <marco@corneelonline.nl>
2022-10-11 13:14:28 +0200

    Refactor pinia lists store

--- src/stores/lists.js
+++ src/stores/lists.js
@@ -1,32 +1,37 @@
-import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 import { v4 as uuid } from "uuid";
 import { todoLists } from "../data.js";
 
-export const useListsStore = defineStore("list", () => {
-  const lists = ref([]);
-  const list = ref({});
-  const error = ref({});
-  const getAll = computed(() => lists);
-  async function fetchLists() {
-    lists.value = [];
-    try {
-      lists.value = [...todoLists];
-    } catch (error) {
-      error.value = error;
-    }
-  }
-  async function fetchList(id) {
-    list.value = null;
-    try {
-      list.value = lists.value.filter((list) => list.id === id).pop();
-    } catch (error) {
-      error.value = error;
-    }
-  }
-  function addList(title) {
-    lists.value.push({ title: title, id: uuid() });
-  }
-
-  return { lists, list, error, getAll, fetchLists, fetchList, addList };
+export const useListsStore = defineStore("list", {
+  state: () => ({
+    lists: [],
+    list: {},
+    error: "",
+  }),
+  getters: {
+    getAll(state) {
+      return state.lists;
+    },
+  },
+  actions: {
+    fetchLists() {
+      this.lists = [];
+      try {
+        this.lists = [...todoLists];
+      } catch (error) {
+        this.error = error;
+      }
+    },
+    fetchList(id) {
+      this.list = null;
+      try {
+        this.list = this.lists.filter((list) => list.id === id).pop();
+      } catch (error) {
+        this.error = error;
+      }
+    },
+    addList(title) {
+      this.lists.push({ title: title, id: uuid() });
+    },
+  },
 });
