4b63187204998b4fbc5f14ef350abcc89ddd8953
Marco Verheul <marco@corneelonline.nl>
2022-10-12 12:54:19 +0200

    Tweak useSidebarOpen composable

--- src/composables/useSidebarOpen.js
+++ src/composables/useSidebarOpen.js
@@ -3,19 +3,13 @@ import { ref } from "vue";
 const globalState = ref(true);
 
 export const useSidebarOpen = () => {
-  const localState = ref(true);
-
   const toggleSidebarOpen = () => {
     // Set the global state values
     globalState.value != globalState.value;
-
-    // Set the local state values
-    localState.value = !localState.value;
   };
 
   return {
     globalState,
-    localState,
     toggleSidebarOpen,
   };
 };
